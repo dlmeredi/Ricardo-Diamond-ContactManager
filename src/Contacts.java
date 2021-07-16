9335d5aa824b9888f0807b81f1c575147c01d257
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-16 11:01:49 -0500

    setting up contacts

---
+++ src/Contacts.java
@@ -0,0 +1,27 @@
+public class Contacts {
+
+    public static String CreateAllContacts(){
+        String contact1 = "John Doe";
+        String contact2 = "Jane Smith";
+        String contact3 = "Ricky Martin";
+        String contact4 = "Britney Spears";
+        String contact5 = "Abraham Lincoln";
+        String contact6 = "Barack Obama";
+        String contact7 = "Michelle Obama";
+        String contact8 = "Steven Spielberg";
+        String contact9 = "Lenny Kravitz";
+        String contact10 = "Batman";
+        String contact11 = "Spider-Man";
+        String contact12 = "Ant Man";
+        String contact13 = "Captain America";
+        String contact14 = "Black Panther";
+        String contact15 = "Captain Marvel";
+
+        return CreateAllContacts();
+    }
+
+    public static String ShowAllContacts(){
+        return CreateAllContacts();
+    }
+
+}
