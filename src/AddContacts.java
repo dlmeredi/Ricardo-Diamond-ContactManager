f37d90d086b8dc1310d5d599849fc4a053b24acf
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-16 22:57:38 -0500

    still working on Delete...

--- src/AddContacts.java
+++ src/AddContacts.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -24,6 +25,16 @@ public class AddContacts {
         //will continue this after we create functionality for search contacts!
 
         return false;
+    }
+
+    public static List<String>CreateContact(){
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.next();
+        List<String> createNewContact = new ArrayList<>();
+        System.out.println("Please create a new contact: ");
+        createNewContact.add(userInput);
+
+        return createNewContact;
     }
 
 }
